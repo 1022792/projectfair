89e83ee6c7b9b82ffe90feb4ee4e8ffc1d2fe42d
patrick <1020156@idcollege.nl>
2018-02-05 13:46:14 +0100

    changed the way how blocks fade in and fade out

--- js/blockanimation.js
+++ js/blockanimation.js
@@ -1,13 +1,43 @@
 
 document.addEventListener('scroll', animation);
 
+
+function handle() {
+
+}
 function animation() {
-    if (document.getElementById('animationStyles').innerHTML !== ''){
-        document.getElementById('animationStyles').innerHTML = '';
+    //TODO: activate this function when scrolled and you got focus on the blocks
+    var blocksleft = document.getElementsByClassName('expertise__slideleft');
+    var blocksright = document.getElementsByClassName('expertise__slideright');
+    var blockscontent = document.getElementsByClassName('expertise__content');
+
+    for (var i = 0; i<blockscontent.length;i++){
+        blockscontent[i].style.animationDuration = "5s";
     }
-    document.getElementById('animationStyles').innerHTML = "@keyframes slideleft{ from { margin-left: 1500px; } to { margin-left: 400px; } }";
-    document.getElementById('animationStyles').innerHTML += "@keyframes slideright{ from { margin-left:-1500px; } to { margin-left:900px; } }";
+    for (var k = 0; k<blocksleft.length;k++){
+        blocksleft[k].style.animationName = "slideleft";
+    }
+    for (var l = 0; l<blocksright.length;l++){
+        blocksright[l].style.animationName = "slideright";
+    }
+    setTimeout(clear,6000);
+    //TODO: activate clear when scrolled away from the blocks
     console.log("You scrolled");
 }
+
+function clear() {
+    var blocksleft = document.getElementsByClassName('expertise__slideleft');
+    var blocksright = document.getElementsByClassName('expertise__slideright');
+    var blockscontent = document.getElementsByClassName('expertise__content');
+
+    for (var i = 0; i<blockscontent.length;i++){
+        blockscontent[i].style.animationDuration = "";
+    }
+    for (var k = 0; k<blocksleft.length;k++){
+        blocksleft[k].style.animationName = "";
+    }
+    for (var l = 0; l<blocksright.length;l++){
+        blocksright[l].style.animationName = "";
+    }}
 
 
