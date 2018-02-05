d03b0f7a39209a165d8d9d6879cdd28fd1191cc2
GrayNeko <Davidmesman@gmail.com>
2018-02-05 08:35:56 +0100

    Added the block animation for the expertise section

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
         },
         watch: {
             scss: {
-                files: ['**/*.scss'],
+                files: ['**/**/*.scss'],
                 tasks: ['sass'],
                 options: {
                     spawn: false
