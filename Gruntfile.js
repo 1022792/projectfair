96160a55e98dc16c02eff74adb3fb14eaea1ca80
GrayNeko <Davidmesman@gmail.com>
2018-02-06 16:17:36 +0100

    add hover background-color change on blocks

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
         },
         watch: {
             scss: {
-                files: ['**/*.scss'],
+                files: ['**/**/*.scss'],
                 tasks: ['sass'],
                 options: {
                     spawn: false
@@ -43,7 +43,8 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('default', ['sass', 'uglify']);
+    grunt.registerTask('default', ['sass', 'concat', 'uglify']);
+    grunt.registerTask('sasswatch', ['sass', 'watch']);
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
