105ffc9d814d157466f006e712352dfa6d60d20a
patrick <1020156@idcollege.nl>
2018-02-07 10:53:19 +0100

    fix font-family & font-size of top sections

--- main.js
+++ main.js
@@ -58,6 +58,23 @@ function draw() {
     document.querySelector('.purpose__text').style.animation = "borders 3s ease 1 forwards";
 }
 
+var listItems = document.querySelectorAll('.menu__link');
+console.log(listItems.length);
+
+for (var i = 0; i < listItems.length; i++) {
+
+    listItems[i].addEventListener('click', jumpToSection);
+}
+
+function jumpToSection() {
+
+
+    var className = '.'+ this.dataset.test;
+
+    console.log('.section ' + className);
+    document.querySelector(className).scrollIntoView({behavior: "smooth"});
+
+}
 window.onload=function(){
     document.getElementById("button").addEventListener("click", showBoxes);
 };
